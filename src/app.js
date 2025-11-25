d27c7ae212ee9143b52d51425c1786921e00cf8c
aayushi sharma <aayushisharma@bitcot.com>
2025-11-25 11:19:59 +0530

    image upload using cloudinary

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import cartRoutes from './modules/cart/cart.routes.js';
 import orderRoutes from './modules/order/order.routes.js';
 import contactRoutes from './modules/contact/contact.routes.js';
 import { notFoundHandler, errorHandler } from './middleware/error.middleware.js';
+import imageRoutes from './modules/image/image.routes.js';
 
 dotenv.config();
 
@@ -17,7 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/api/image', imageRoutes);
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
