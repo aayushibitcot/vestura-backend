47de3ee515cec338148590743832dff517345595
aayushi sharma <aayushisharma@bitcot.com>
2025-12-05 12:39:54 +0530

    fix: restore avatar field in updateUserSchema validation

--- src/modules/user/user.validation.js
+++ src/modules/user/user.validation.js
@@ -4,8 +4,8 @@ export const updateUserSchema = Joi.object({
   firstName: Joi.string().optional(),
   lastName: Joi.string().optional(),
   phone: Joi.string().optional(),
-  // avatar: Joi.string().allow(null, '').optional(),
-});
+  avatar: Joi.string().allow(null, '').optional(),
+}).unknown(true);
 
 
 
